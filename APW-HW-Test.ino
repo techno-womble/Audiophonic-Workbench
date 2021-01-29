f73c904da7a18b31fe60fe37e9aac90f4fddc028
techno-womble <54324262+techno-womble@users.noreply.github.com>
2021-01-29 17:26:21 +0000

    Add files via upload

---
+++ APW-HW-Test.ino
@@ -0,0 +1,99 @@
+//  Sketch to test the AudioPhonic Workbench Hardware. 
+//   
+//   Generate a test tone on using the Mozzi HI-FI
+//   PWM mode (pins 9 & 10 combined).
+//   Read each analogue input in turn and display
+//   the values via the serial terminal
+//   
+//   Read and display the switch states.
+//
+//   *************************************** IMPORTANT ***********************************************
+//
+//   This sketch requires Mozzi/mozzi_config.h to be
+//   be changed from STANDARD mode to HIFI.
+//
+//   In Mozz/mozzi_config.h, change
+//   #define AUDIO_MODE STANDARD
+//   //#define AUDIO_MODE HIFI to
+//   //#define AUDIO_MODE STANDARD
+//   #define AUDIO_MODE HIFI
+//
+//
+//  John Potter (technoWomble), 2021 CC by-nc-sa.
+
+
+#include <MozziGuts.h>
+#include <Oscil.h> 
+#include <tables/sin2048_int8.h>                      // Oscillator sine wave table
+
+
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> testOsc(SIN2048_DATA);    
+
+#define CONTROL_RATE 1    
+#define AUDIO_MODE HIFI                              
+
+const int vr1Pin = A0;
+const int vr2Pin = A1;
+const int vr3Pin = A2;
+const int vr4Pin = A3;
+const int vr5Pin = A4;
+const int vr6Pin = A5;
+const int vr7Pin = A6;
+const int vr8Pin = A7;
+
+const int sw1Pin = 2;
+const int sw2Pin = 3;
+
+
+void setup(){
+  startMozzi(CONTROL_RATE); 
+  testOsc.setFreq(440);                               // set the frequency
+  Serial.begin(9600);
+  pinMode(sw1Pin, INPUT_PULLUP); 
+  pinMode(sw2Pin, INPUT_PULLUP);  
+}
+
+
+void updateControl(){
+   int potVal1 = mozziAnalogRead(vr1Pin);
+   Serial.print(potVal1);
+   Serial.print(" - ");
+   int potVal2 = mozziAnalogRead(vr2Pin);
+   Serial.print(potVal2);
+   Serial.print(" - ");
+   int potVal3 = mozziAnalogRead(vr3Pin);
+   Serial.print(potVal3);
+   Serial.print(" - ");
+   int potVal4 = mozziAnalogRead(vr4Pin);
+   Serial.print(potVal4);
+   Serial.print(" - ");
+   int potVal5 = mozziAnalogRead(vr5Pin);
+   Serial.print(potVal5);
+   Serial.print(" - ");
+   int potVal6 = mozziAnalogRead(vr6Pin);
+   Serial.print(potVal6);
+   Serial.print(" - ");
+   int potVal7 = mozziAnalogRead(vr7Pin);
+   Serial.print(potVal7);
+   Serial.print(" - ");
+   int potVal8 = mozziAnalogRead(vr8Pin);
+   Serial.print(potVal8);
+   Serial.print(" - ");
+
+int swVal1 = digitalRead(sw1Pin);
+   Serial.print(swVal1 );
+   Serial.print(" - ");
+int swVal2 = digitalRead(sw2Pin);
+   Serial.print(swVal2 );
+   Serial.println(" "); 
+}
+
+
+int updateAudio(){
+  return testOsc.next() << 5;                           // scale the output for HIFI mode
+}
+
+void loop() {
+
+audioHook();                                           // required here
+}
---
+++ APW-Sketch.ino
@@ -0,0 +1,128 @@
+//  AudioPhonic WorkBench Sketch. 
+// 
+//  Simulate three vintage style test oscillators
+//  with amplitude and frequency cross modulation
+//
+//   *************************************** IMPORTANT ***********************************************
+//
+//   This sketch requires Mozzi/mozzi_config.h to be
+//   be changed from STANDARD mode to HIFI.
+//
+//   In Mozz/mozzi_config.h, change
+//   #define AUDIO_MODE STANDARD
+//   //#define AUDIO_MODE HIFI to
+//   //#define AUDIO_MODE STANDARD
+//   #define AUDIO_MODE HIFI
+//
+//   
+//  John Potter (technoWomble), 2021 CC by-nc-sa.
+//
+
+#include <MozziGuts.h>
+#include <Oscil.h> 
+#include <tables/sin2048_int8.h>    
+
+#define AUDIO_RATE 32768
+#define CONTROL_RATE 128
+
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc1(SIN2048_DATA);  
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc2(SIN2048_DATA);  
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> osc3(SIN2048_DATA);
+
+const int vr1Pin = A0;
+const int vr2Pin = A1;
+const int vr3Pin = A2;
+const int vr4Pin = A3;
+const int vr5Pin = A4;
+const int vr6Pin = A5;
+const int vr7Pin = A6;
+const int vr8Pin = A7;
+
+const int sw1Pin = 2;
+const int sw2Pin = 3;
+
+int osc1Freq;
+int osc2Freq;
+int osc3Freq;
+
+int osc1Level;
+int osc2Level;
+int osc3Level;
+
+int osc2FMVal;
+int osc3FMVal;
+ 
+int osc2AMVal;
+int osc3AMVal; 
+
+void setup(){
+  pinMode(sw1Pin, INPUT_PULLUP); 
+  pinMode(sw2Pin, INPUT_PULLUP); 
+  startMozzi(CONTROL_RATE); 
+  osc1.setPhase(random(2047));                            // don't want the oscillators locked in phase
+  osc2.setPhase(random(2047));
+  osc3.setPhase(random(2047)); 
+}
+
+void updateControl(){
+  osc2FMVal = 0;
+  osc3FMVal = 0;
+  osc2AMVal = 0;
+  osc3AMVal = 0;
+  
+  osc1Freq = ((mozziAnalogRead (vr1Pin)) << 3) + 128;      // Osc1 Pitch, 
+  osc2Freq = ((mozziAnalogRead (vr2Pin)) << 3) + 128;      // Osc2 Pitch    
+  osc3Freq = ((mozziAnalogRead (vr3Pin)) << 3) + 128;      // Osc3 Pitch    
+
+  osc1Level = mozziAnalogRead(vr4Pin) >> 2;                // Osc1 Volume, 0 - 255        
+  osc2Level = mozziAnalogRead(vr5Pin) >> 2;                // Osc2 Volume
+  osc3Level = mozziAnalogRead(vr6Pin) >> 2;                // Osc3 Volume
+  
+  int pot7Val = mozziAnalogRead(vr7Pin);                   // OSC2 MODULATION    
+  if (pot7Val <= 512) {osc2FMVal = (512 - pot7Val);} else  // Osc2 FM amount, 0 to 511 
+                      {osc2AMVal = (pot7Val- 512) >> 2 ;}  // Osc2 AM amount, 0 to 127                                                                             
+
+  int pot8Val = mozziAnalogRead(vr8Pin);                   // OSC3 MODULATION
+  if (pot8Val <= 512) {osc3FMVal = (512 - pot8Val);} else  // Osc3 FM amount, 0 to 511  
+                      {osc3AMVal = (pot8Val- 512) >> 2;}   // Osc3 AM amount, 0 to 127             
+   
+  if (digitalRead(sw1Pin)==0) {osc1Freq = osc1Freq >> 7;}  // Osc1 high/low rate switch  
+  if (digitalRead(sw2Pin)==0) {osc2Freq = osc2Freq >> 7;}  // Osc2 high/low rate switch  
+
+  osc1Freq = constrain(osc1Freq,1,8194);
+  osc1.setFreq(osc1Freq); 
+}
+
+int updateAudio(){
+  int osc1Val = osc1.next();
+
+  int osc2ModulatedFreq = (osc2Freq + ((osc1Val ) * (osc2FMVal >> 4)));        // calculate Osc2 FM           
+  osc2ModulatedFreq = constrain(osc2ModulatedFreq,1,8194);
+  osc2.setFreq(osc2ModulatedFreq); 
+
+  int osc2Val = osc2.next();
+  
+  int osc2Trem = (osc1Val + 128L);                                              // calculate Osc2 AM    
+  osc2Trem = 255 - ((osc2Trem * osc2AMVal) >> 7) ;
+  osc2Val = (osc2Val * osc2Trem) >> 8;
+  
+  int osc3ModulatedFreq = (osc3Freq + ((osc2Val ) * (osc3FMVal >> 4)));        // calculate Osc3 FM                        
+  osc3ModulatedFreq = constrain(osc3ModulatedFreq,1,8194);        
+  osc3.setFreq(osc3ModulatedFreq);  
+
+  int osc3Val = osc3.next(); 
+ 
+  int osc3Trem = (osc2Val + 128);                                              // calculate Osc2 AM  
+  osc3Trem = 255 - ((osc3Trem * osc3AMVal) >> 7) ;
+  osc3Val = (osc3Val * osc3Trem) >> 8;
+
+  int osc1Out = (osc1Val * osc1Level) >> 3;
+  int osc2Out = (osc2Val * osc2Level) >> 3;                                    // set each oscillator volume                        
+  int osc3Out = (osc3Val * osc3Level) >> 3;
+    
+  return (osc1Out + osc2Out + osc3Out) >> 1;                                  // mix and output                 
+}
+
+void loop(){
+  audioHook();                                                                 // required according to Mozzi docs
+}
